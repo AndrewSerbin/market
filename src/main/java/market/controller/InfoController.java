d4744a83890a8bb98d8ea37ed1a5c56db9306678
AndrewSerbin <andrewserbin01@gmail.com>
2018-11-24 13:49:47 +0200

    Add init sql

--- src/main/java/market/controller/InfoController.java
+++ src/main/java/market/controller/InfoController.java
@@ -3,15 +3,10 @@ package market.controller;
 import market.entity.Id;
 import market.entity.Purchase;
 import market.entity.PizzaInfo;
-import market.entity.PricePerWeight;
 import market.service.PurchaseService;
 import market.service.PizzaInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 public class InfoController {
@@ -21,88 +16,6 @@ public class InfoController {
     @Autowired
     private PurchaseService purchaseService;
 
-    @GetMapping(value = "/health")
-    public String health() {
-        return "Healthy";
-    }
-
-    @GetMapping(value = "/init")
-    public void init() {
-        pizzaInfoService.save(getInitData());
-    }
-
-    private List<PizzaInfo> getInitData() {
-        List<PizzaInfo> pizzaInfos = new ArrayList<>();
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Пеперони с  томатами")
-                .ingridients(Arrays.asList("моцарелла", "пеперони", "помидоры","соус барбекю"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 90),
-                        new PricePerWeight(30, 150),
-                        new PricePerWeight(36, 189)
-                ))
-                .imgUrl("/0001.jpg")
-                .build());
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Техас")
-                .ingridients(Arrays.asList("кукуруза", "моцарелла", "лук", "грибы", "колбаски баварские", "соус барбекю"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 80),
-                        new PricePerWeight(30, 154),
-                        new PricePerWeight(36, 189)
-                ))
-                .imgUrl("/0002.jpg")
-                .build());
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Гавайская")
-                .ingridients(Arrays.asList("курица", "моцарелла", "ананас", "фирменный соус"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 109),
-                        new PricePerWeight(30, 169),
-                        new PricePerWeight(36, 214)
-                ))
-                .imgUrl("/0003.jpg")
-                .build());
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Прованс")
-                .ingridients(Arrays.asList("моцарелла", "бергадер блю", "шинка", "пеперони", "помидоры", "соус альфредо"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 129),
-                        new PricePerWeight(30, 194),
-                        new PricePerWeight(36, 224)
-                ))
-                .imgUrl("/0004.jpg")
-                .build());
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Пеперони блюз")
-                .ingridients(Arrays.asList("моцарелла", "бергадер блю", "пеперони",  "соус альфредо"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 109),
-                        new PricePerWeight(30, 164),
-                        new PricePerWeight(36, 214)
-                ))
-                .imgUrl("/0005.jpg")
-                .build());
-
-        pizzaInfos.add(PizzaInfo.builder()
-                .name("Пицца Пять сыров")
-                .ingridients(Arrays.asList("фета", "моцарелла", "пармезан",  "бергадер блю", "соус альфредо" , "чедер"))
-                .pricePerWeight(Arrays.asList(
-                        new PricePerWeight(22, 164),
-                        new PricePerWeight(30, 234),
-                        new PricePerWeight(36, 269)
-                ))
-                .imgUrl("/0006.jpg")
-                .build());
-
-        return pizzaInfos;
-    }
-
     @GetMapping(value = "/pizzas", produces = "application/json")
     public Iterable<PizzaInfo> findAll() {
         return pizzaInfoService.findAll();
@@ -111,7 +24,6 @@ public class InfoController {
     @PostMapping(value = "/purchases", consumes = "application/json", produces = "application/json")
     public Id save(@RequestBody Purchase purchase) {
         purchaseService.save(purchase);
-        System.out.println(purchase);
         return new Id(purchase.getId());
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,71 @@
+insert into pizza_info (id, img_url, name) values (1, '/0001.jpg', 'Пицца Пеперони с  томатами')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (1, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (1, 'пеперони')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (1, 'помидоры')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (1, 'соус барбекю')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (1, '90', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (1, '150', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (1, '189', '36')
+
+insert into pizza_info (id, img_url, name) values (2, '/0002.jpg', 'Пицца Техас')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'кукуруза')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'лук')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'грибы')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'колбаски баварские')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (2, 'соус барбекю')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (2, '80', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (2, '154', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (2, '189', '36')
+
+insert into pizza_info (id, img_url, name) values (3, '/0003.jpg', 'Пицца Гавайская')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (3, 'курица')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (3, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (3, 'ананас')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (3, 'фирменный соус')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (3, '109', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (3, '169', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (3, '214', '36')
+
+insert into pizza_info (id, img_url, name) values (4, '/0004.jpg', 'Пицца Прованс')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'бергадер блю')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'шинка')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'пеперони')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'помидоры')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (4, 'соус альфредо')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (4, '129', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (4, '194', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (4, '224', '36')
+
+insert into pizza_info (id, img_url, name) values (5, '/0005.jpg', 'Пицца Пеперони блюз')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (5, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (5, 'бергадер блю')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (5, 'пеперони')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (5, 'соус альфредо')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (5, '109', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (5, '164', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (5, '214', '36')
+
+insert into pizza_info (id, img_url, name) values (6, '/0006.jpg', 'Пицца Пять сыров')
+
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'фета')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'моцарелла')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'пармезан')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'бергадер блю')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'соус альфредо')
+insert into pizza_info_ingridients (pizza_info_id, ingridients) values (6, 'чедер')
+
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (6, '164', '22')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (6, '234', '30')
+insert into pizza_info_price_per_weight (pizza_info_id, price, size) values (6, '269', '36')
